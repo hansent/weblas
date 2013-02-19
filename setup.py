c75472f837816fd36601dc85da1e2874c4d7da09
Howard Butler <hobu.inc@gmail.com>
2013-02-19 17:09:03 -0600

    rename to plasio

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import shutil
 
-import plas
+import plasio
 
 # Get text from README.txt
 try:
@@ -10,8 +10,8 @@ except:
     readme_text = "See documentation at plas.io"
 
 
-setup(name          = 'plas',
-      version       = plas.__version__,
+setup(name          = 'plasio',
+      version       = plasio.__version__,
       description   = 'Python LiDAR server',
       license       = 'BSD',
       keywords      = 'gis lidar las',
@@ -19,8 +19,8 @@ setup(name          = 'plas',
       author_email  = 'thomas@fresklabs.com',
       url   = 'https://github.com/grantbrown/laspy',
       long_description = '''Some descriptoin''',
-      packages      = ['plas', ],
-      install_requires = ['numpy'],
+      packages      = ['plasio', ],
+      install_requires = ['numpy', 'psycopg2'],
       test_suite = 'test.test_plas',
       # data_files = [("laspytest/data", ["simple.las", "simple1_3.las", "simple1_4.las"])], 
       data_files = [], 
